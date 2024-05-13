899f002ec9b5040fc21dbbbeb00c5c04c83d31af
SeoHaeYun <SeoHaeYun@github.com>
2024-05-13 21:30:24 +0900

    fix: bottom navigation view 수정

--- app/src/main/java/kr/camp/youtube/MainActivity.kt
+++ app/src/main/java/kr/camp/youtube/MainActivity.kt
@@ -3,30 +3,26 @@ package kr.camp.youtube
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kr.camp.youtube.databinding.ActivityMainBinding
-import java.lang.IndexOutOfBoundsException
+import kr.camp.youtube.view.search.SearchFragment
 
 class  MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    var currentPosition = 0
-
-    val whiteIcon = listOf(
+    val whiteIcons = listOf(
         R.drawable.home_white,
         R.drawable.search_white,
         R.drawable.youtube_white
     )
 
-    val blackIcon = listOf(
+    val blackIcons = listOf(
         R.drawable.home_black,
         R.drawable.search_black,
         R.drawable.youtube_black
     )
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,30 +41,28 @@ class  MainActivity : AppCompatActivity() {
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
 
-
-        val tabIcons = if (currentPosition == 0) blackIcon else whiteIcon
-
         viewPager.adapter = ViewPagerAdapter(fragmentList, this@MainActivity)
 
         // ViewPager2 - TabLayout
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.setIcon(whiteIcon[position])
+            tab.setIcon(whiteIcons[position])
         }.attach()
 
         // TabLayout Click Event (Icon Change)
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                currentPosition = position
-                tabLayout.getTabAt(position)?.setIcon(getIconResource(position))
+                for (i in 0 until tabLayout.tabCount) {
+                    tabLayout.getTabAt(i)?.setIcon(getIconResource(i, position))
                 }
+            }
         })
     }
-    private fun getIconResource(position: Int): Int {
-        return if (currentPosition == position) blackIcon[position] else whiteIcon[position]
-    }
-}
 
+    private fun getIconResource(tabIndex: Int, currentPosition: Int): Int {
+        return if (tabIndex == currentPosition) blackIcons[tabIndex] else whiteIcons[tabIndex]
+        }
+    }
 
 
 
