b061baf3d8272e59411f885c8c2bc06cde4db1e3
Hyuna <kmtso434@naver.com>
2024-05-17 14:39:57 +0900

    리사이클러뷰 구현

--- app/src/main/java/kr/camp/youtube/MainActivity.kt
+++ app/src/main/java/kr/camp/youtube/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kr.camp.youtube.databinding.ActivityMainBinding
+import kr.camp.youtube.myVideo.state.MyVideoFragment
 import kr.camp.youtube.view.search.SearchFragment
 
 class  MainActivity : AppCompatActivity() {
