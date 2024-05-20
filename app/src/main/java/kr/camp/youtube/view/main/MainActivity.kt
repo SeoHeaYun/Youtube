c4b0c1c0d6ed8ca63c174f1430f2947823657640
Hyuna <kmtso434@naver.com>
2024-05-20 20:24:54 +0900

    merge conflict 수정

--- app/src/main/java/kr/camp/youtube/view/main/MainActivity.kt
+++ app/src/main/java/kr/camp/youtube/view/main/MainActivity.kt
@@ -1,12 +1,11 @@
 package kr.camp.youtube.view.main
 
 import android.os.Bundle
-import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
-import kr.camp.youtube.view.myvideo.MyVideoFragment
+import kr.camp.youtube.view.myVideo.state.MyVideoFragment
 import kr.camp.youtube.R
 import kr.camp.youtube.databinding.ActivityMainBinding
 import kr.camp.youtube.view.home.HomeFragment
--- app/src/main/java/kr/camp/youtube/view/myVideo/state/MyVideoFragment.kt
+++ app/src/main/java/kr/camp/youtube/view/myVideo/state/MyVideoFragment.kt
@@ -1,17 +1,11 @@
 package kr.camp.youtube.view.myVideo.state
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 import kr.camp.youtube.BuildConfig
 import kr.camp.youtube.R
