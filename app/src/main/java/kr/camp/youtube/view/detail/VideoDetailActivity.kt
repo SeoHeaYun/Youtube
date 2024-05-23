e6b01aad4037a8450e6b01a0310e62d0662229de
bora <bora44144@gmail.com>
2024-05-23 11:47:04 +0900

    feat: like 기능 구현중

--- app/src/main/java/kr/camp/youtube/view/detail/VideoDetailActivity.kt
+++ app/src/main/java/kr/camp/youtube/view/detail/VideoDetailActivity.kt
@@ -1,22 +1,33 @@
 package kr.camp.youtube.view.detail
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import kr.camp.youtube.R
 import kr.camp.youtube.databinding.ActivityVideoDetailBinding
 import kr.camp.youtube.extension.toSpanned
-import kr.camp.youtube.view.intent.IntentKey
 import kr.camp.youtube.view.detail.model.DummyDataManager
+import kr.camp.youtube.view.intent.IntentKey
 import kr.camp.youtube.view.detail.model.LikeItemModel
 import kr.camp.youtube.view.intent.item.DetailItem
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class VideoDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityVideoDetailBinding.inflate(layoutInflater) }
     private lateinit var item: LikeItemModel
+    // 좋아요를 눌러 선택된 아이템을 저장하는 리스트
+    private var likedItems: ArrayList<LikeItemModel> = ArrayList()
+    private var stringPrefs : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +35,8 @@ class VideoDetailActivity : AppCompatActivity() {
 
         overridePendingTransition(R.anim.from_down_enter, R.anim.none)
         setupView()
+
+        getPrefLikeItems("likeditems")
     }
 
     override fun onBackPressed() {
@@ -54,35 +67,81 @@ class VideoDetailActivity : AppCompatActivity() {
 
         titleTextView.text = detailItem.videoTitle.toSpanned()
         descriptionTextView.text = detailItem.videoDescription.toSpanned()
-        likeButton.text = if (item.isLike) "UnLike" else "Like"
+        likeButton.text = if (item.isLike) "Like" else "UnLike"
 
         likeButton.setOnClickListener{
-            item.changeLike()
-            binding.likeButton.text = if (item.isLike) "UnLike" else "Like"
-            updateDummyData()
+            if (!item.isLike) {
+                addPrefItem("likeditems", likedItems)
+                item.isLike = true
+                likeButton.text = "LIKE"
+                Log.d("TAG", likedItems.toString())
+            } else {
+                deletePrefItem(item.thumbnailUrl)
+                item.isLike = false
+                likeButton.text = "UNLIKE"
+                Log.d("TAG2", likedItems.toString())
+            }
         }
+
 
         buttonBack.setOnClickListener{
             val resultIntent = Intent().apply {
                 putExtra(IntentKey.DETAIL_ITEM, item)
             }
             setResult(Activity.RESULT_OK, resultIntent)
-            finish()
+
+            super.finish()
+            overridePendingTransition(R.anim.none, R.anim.to_down_exit)
         }
     }
 
-    private fun updateDummyData(){
-        val dummyData = DummyDataManager.getDummyData().toMutableList()
-        val index = dummyData.indexOfFirst { it.videoTitle == item.videoTitle }
-        if (index != -1) {
-            dummyData[index] = item
-            DummyDataManager.updateDummyData(dummyData)
+    fun addPrefItem(key: String, items: ArrayList<LikeItemModel>) {
+        val sharedPreferences = getSharedPreferences("shared preferences", Context.MODE_PRIVATE)
+        val edit = sharedPreferences.edit()
+        val jsonArray = JSONArray()
+        val gson = GsonBuilder().create()
+
+        for (i in 0 until items.size) {
+            val string = gson.toJson(items.get(i), LikeItemModel::class.java)
+            jsonArray.put(string)
+            Log.d("TAG3", string)
         }
+
+        if (!items.isEmpty()) {
+            edit.putString(key, gson.toJson(item.thumbnailUrl))
+        } else {
+            edit.putString(key, null)
+        }
+
+        edit.apply()
     }
 
+    fun deletePrefItem(thumbnailUrl: String) {
+        val sharedPreferences = getSharedPreferences("shared preferences", Context.MODE_PRIVATE)
+        val edit = sharedPreferences.edit()
+        edit.remove(thumbnailUrl)
+        edit.apply()
+    }
 
-    // 좋아요를 눌러 선택된 아이템을 저장하는 리스트
-    var likedItems: ArrayList<LikeItemModel> = ArrayList()
+    fun getPrefLikeItems(key: String) : ArrayList<LikeItemModel> {
+        val sharedPreferences = getSharedPreferences("shared preferences", Context.MODE_PRIVATE)
+        val json = sharedPreferences.getString(key, null)
+        val likeditems = ArrayList<LikeItemModel>()
+        val gson = GsonBuilder().create()
+
+        if (json != null) {
+            try {
+                val jsonArray = JSONArray(json)
+                for (i in 0 until jsonArray.length()) {
+                    val likeitem = gson.fromJson(jsonArray.getJSONObject(i).toString(), LikeItemModel::class.java)
+                    likeditems.add(likeitem)
+                }
+            } catch (e:JSONException) {
+                e.printStackTrace()
+            }
+        }
+        return likeditems
+    }
 
     /**
      * 좋아요가 눌린 아이템을 likedItems 리스트에 추가하는 함수입니다.
@@ -101,8 +160,4 @@ class VideoDetailActivity : AppCompatActivity() {
         likedItems.remove(item)
     }
 
-
 }
-
-
-
